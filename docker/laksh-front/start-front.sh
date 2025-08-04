f1bec4970b0301f112deb1fd538be4872dc5268f
Igon <mtgsale@mail.ru>
2025-08-04 22:43:08 +0300

    Build fix

--- docker/laksh-front/start-front.sh
+++ docker/laksh-front/start-front.sh
@@ -3,6 +3,14 @@
 # Переходим в директорию проекта
 cd /app
 
+# Очищаем папку перед клонированием
+echo "Очистка папки..."
+rm -rf /app/* /app/.[^.]*
+
+# Клонируем репозиторий фронтенда
+echo "Клонирование репозитория фронтенда..."
+git clone https://github.com/IgonRU/laksh-front.git .
+
 # Устанавливаем зависимости
 echo "Установка зависимостей..."
 npm install
@@ -16,6 +24,16 @@ echo "Копирование файлов в nginx..."
 mkdir -p /usr/share/nginx/html/browser
 cp -r /app/dist/laksh-front/browser/* /usr/share/nginx/html/browser/
 
+# Копируем файлы в общий volume
+echo "Копирование файлов в общий volume..."
+cp -r /app/dist/laksh-front/browser/* /shared-frontend/
+
 # Запускаем nginx
 echo "Запуск nginx..."
-nginx -g "daemon off;" 
+nginx -g "daemon off;" &
+echo "Nginx запущен в фоновом режиме"
+
+# Ждем бесконечно
+while true; do
+  sleep 1
+done 
