6deb529ff501f41574379473eaa01ad13a40298f
Igon <mtgsale@mail.ru>
2025-09-01 10:31:07 +0300

    Front navigation fix
    Nginx crashed internal angular navigation - fixed

--- volumes/frontend/src/app/_shared/build-info.ts
+++ volumes/frontend/src/app/_shared/build-info.ts
@@ -2,9 +2,9 @@
 // Не редактируйте его вручную!
 
 export const BUILD_INFO = {
-  "buildTime": "2025-08-31T19:41:48.685Z",
-  "buildDate": "31.08.2025, 19:41:48",
-  "buildTimestamp": 1756669308685,
+  "buildTime": "2025-09-01T07:08:52.372Z",
+  "buildDate": "01.09.2025, 07:08:52",
+  "buildTimestamp": 1756710532372,
   "commitHash": "unknown",
   "branch": "unknown",
   "lastTag": "unknown",
